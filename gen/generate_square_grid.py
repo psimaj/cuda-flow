82ae5493f4524d342e0b05d70a465d2f03baf3bc
Przemyslaw Simajchel <z1143652@miracle.tcs.uj.edu.pl>
2019-01-22 02:02:44 +0100

    Bugfix random square grid generation

--- gen/generate_square_grid.py
+++ gen/generate_square_grid.py
@@ -30,18 +30,18 @@ max_edge_cap = int(sys.argv[2])
 
 print(n, m, 1, n**2)
 
-for i in range(n):
+for i in range(1, n+1):
     for j in range(1, n):
-        cap = math.ceil(max_edge_cap * scaling_factor(i+1, j, n))
+        cap = math.ceil(max_edge_cap * scaling_factor(i, j, n))
         if random.random() < right_down_probability:
-            print(i * n + j, i * n + j + 1, random.randint(0, cap))
+            print((i-1) * n + j, (i-1) * n + j + 1, random.randint(0, cap))
         else:
-            print(i * n + j + 1, i * n + j, random.randint(0, cap))
+            print((i-1) * n + j + 1, (i-1) * n + j, random.randint(0, cap))
 
-for i in range(n):
-    for j in range(1, n):
-        cap = math.ceil(max_edge_cap * scaling_factor(i+1, j, n))
+for i in range(1, n):
+    for j in range(1, n+1):
+        cap = math.ceil(max_edge_cap * scaling_factor(i, j, n))
         if random.random() < right_down_probability:
-            print(i * n + j, (i + 1) * n + j, random.randint(0, cap))
+            print((i-1) * n + j, i * n + j, random.randint(0, cap))
         else:
-            print((i + 1) * n + j, i * n + j, random.randint(0, cap))
+            print(i * n + j, (i-1) * n + j, random.randint(0, cap))
